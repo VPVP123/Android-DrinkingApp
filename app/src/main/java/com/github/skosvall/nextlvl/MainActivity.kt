c1793c64a58f7c55a1243691a13a0afe0b547e13
Filip Larsson <flarsson1999@gmail.com>
2021-02-08 17:18:41 +0100

    Addded buttons

--- app/src/main/java/com/github/skosvall/nextlvl/MainActivity.kt
+++ app/src/main/java/com/github/skosvall/nextlvl/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.github.skosvall.nextlvl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val mobileGamesBtn = findViewById<Button>(R.id.button)
+        val cardGamesBtn = findViewById<Button>(R.id.button2)
+        val lvlGamesBtn = findViewById<Button>(R.id.button3)
+
+        mobileGamesBtn.setOnClickListener {
+            val intent = Intent(this, mobileGamesActivity::class.java)
+            startActivity(
+                intent
+            )
+        }
+
+        cardGamesBtn.setOnClickListener {
+            val intent = Intent(this, cardGamesActivity::class.java)
+            startActivity(
+                intent
+            )
+        }
+
+        lvlGamesBtn.setOnClickListener {
+            val intent = Intent(this, lvlGamesActivity::class.java)
+            startActivity(
+                intent
+            )
+        }
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/github/skosvall/nextlvl/cardGamesActivity.kt
@@ -0,0 +1,11 @@
+package com.github.skosvall.nextlvl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cardGamesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_games)
+    }
+}
