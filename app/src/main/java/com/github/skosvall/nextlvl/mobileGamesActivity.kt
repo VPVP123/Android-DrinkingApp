6c74d066bbf8c498c8ae514b93fce0c81a160f29
Filip Larsson <flarsson1999@gmail.com>
2021-02-10 17:10:41 +0100

    Added prepareDareOrDrink

--- app/src/main/java/com/github/skosvall/nextlvl/mobileGamesActivity.kt
+++ app/src/main/java/com/github/skosvall/nextlvl/mobileGamesActivity.kt
@@ -1,11 +1,23 @@
 package com.github.skosvall.nextlvl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class mobileGamesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mobile_games)
+
+        val DOR_Btn = findViewById<Button>(R.id.DOR_button)
+
+
+        DOR_Btn.setOnClickListener {
+            val intent = Intent(this, PrepareDareOrDrinkActivity::class.java)
+            startActivity(
+                intent
+            )
+        }
     }
 }
