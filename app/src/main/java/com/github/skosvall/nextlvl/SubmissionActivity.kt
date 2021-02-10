7a672cb6896f35b6283384bb5a98649a83c8f0aa
Victor Svanqvist <svvi19ty@student.ju.se>
2021-02-10 17:13:36 +0100

    Update SubmissionActivity.kt

--- app/src/main/java/com/github/skosvall/nextlvl/SubmissionActivity.kt
+++ app/src/main/java/com/github/skosvall/nextlvl/SubmissionActivity.kt
@@ -1,5 +1,7 @@
 package com.github.skosvall.nextlvl
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -12,14 +14,33 @@ class SubmissionActivity : AppCompatActivity() {
 
         val listView = this.findViewById<ListView>(R.id.submissionList)
 
-        listView.adapter = ArrayAdapter<Submission>(
+        var adapter = ArrayAdapter<Submission>(
             this,
             android.R.layout.simple_list_item_1,
             android.R.id.text1,
             submissionRepository.getAllSubmissions()
         )
+        listView.adapter = adapter
 
+        listView.setOnItemClickListener {parent, view, position, id ->
 
+            val submission = listView.adapter.getItem(position) as Submission
 
+            val popUpError1 = AlertDialog.Builder(this)
+            popUpError1.setTitle("Submission")
+            popUpError1.setMessage("This is a test")
+            popUpError1.setNeutralButton( "Submit") { dialog, which ->
+                dialog.dismiss()
+            }
+            popUpError1.setPositiveButton( "Remove") { dialog, which ->
+                submissionRepository.deleteSubmissionById(submission.id)
+                adapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+            popUpError1.setNegativeButton( "Edit") { dialog, which ->
+                dialog.dismiss()
+            }
+            popUpError1.show()
+        }
     }
 }
