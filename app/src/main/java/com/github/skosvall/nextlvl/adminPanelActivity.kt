1b825f95b7a26c4da6053d1d5cb19a38cc32c303
Victor Svanqvist <svvi19ty@student.ju.se>
2021-02-10 14:05:43 +0100

    Added admin panel

---
+++ app/src/main/java/com/github/skosvall/nextlvl/adminPanelActivity.kt
@@ -0,0 +1,11 @@
+package com.github.skosvall.nextlvl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class adminPanelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_panel)
+    }
+}
