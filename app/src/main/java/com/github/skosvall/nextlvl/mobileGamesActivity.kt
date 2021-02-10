0d424fe4a5cebc51fdcb676da4c132b358a4661b
HugoMartinsson <43168092+HugoMartinsson@users.noreply.github.com>
2021-02-10 17:07:10 +0100

    Added playNeverHaveIEver activity

--- app/src/main/java/com/github/skosvall/nextlvl/mobileGamesActivity.kt
+++ app/src/main/java/com/github/skosvall/nextlvl/mobileGamesActivity.kt
@@ -1,11 +1,18 @@
 package com.github.skosvall.nextlvl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class mobileGamesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mobile_games)
+
+        val neverHaveIEverButton = findViewById<Button>(R.id.neverHaveIEverButton)
+        neverHaveIEverButton.setOnClickListener {
+            startActivity(Intent(this, PlayNeverHaveIEverActivity::class.java))
+        }
     }
 }
