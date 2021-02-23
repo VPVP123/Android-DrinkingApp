931c1a3ab42a25789ac37ef27d03d45408be59c4
Victor Svanqvist <svvi19ty@student.ju.se>
2021-02-23 15:26:30 +0100

    added spinner with transparent background in addsubmission

--- app/src/main/java/com/github/skosvall/nextlvl/AddSubmissionActivity.kt
+++ app/src/main/java/com/github/skosvall/nextlvl/AddSubmissionActivity.kt
@@ -2,10 +2,28 @@ package com.github.skosvall.nextlvl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class AddSubmissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_submission)
+
+        val spinner: Spinner = findViewById(R.id.sip_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.sipArray,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
+
     }
 }
